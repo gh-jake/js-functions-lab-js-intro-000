1f56638cf90ca2a2851d25f97a680298ecb5a261
Jake Houston <jakechouston@gmail.com>
2021-02-05 00:00:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function happyHolidays() {
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays to ${name}!`
+  return `Happy holidays to ${name}!`;
 }
 
 function happyCustomHolidayTo(holiday, name) {
-  return  `Happy ${holiday}, ${name}!`
+  return  `Happy ${holiday}, ${name}!`;
 }
